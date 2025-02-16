5cdac9f13b6d53a684bed7242dd1db8898c8a9ea
denis <dmstarikov@edu.hse.ru>
2025-02-16 22:32:42 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+let box = document.querySelectorAll(".clickbox");
+box.forEach(function(clickbox){
+    clickbox.addEventListener("click", function(){
+        if(clickbox.style.backgroundColor === "lightsteelblue"){
+            clickbox.style.backgroundColor = "yellow";
+            clickbox.style.margin = '2vw';
+            console.log("change color");
+        }
+        else{
+            clickbox.style.backgroundColor = "lightsteelblue";
+            clickbox.style.margin = '1vw';
+        }
+    });
+});
